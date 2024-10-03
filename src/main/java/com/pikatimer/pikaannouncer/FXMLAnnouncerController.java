95d1b76c9b17ada92f5349603ab236f6e4d12a64
John Garner <segfaultcoredump@gmail.com>
2024-10-02 19:45:25 -0600

    Fix package name

--- src/main/java/com/pikatimer/pikaannouncer/FXMLAnnouncerController.java
+++ src/main/java/com/pikatimer/pikaannouncer/FXMLAnnouncerController.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.pikareader.pikaannouncer;
+package com.pikatimer.pikaannouncer;
 
 import de.siegmar.fastcsv.reader.CsvReader;
 import java.io.BufferedReader;
